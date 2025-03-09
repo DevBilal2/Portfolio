98193172d3b7cd6d81df6b16582fe5d06d19c375
DevBilal2 <m.bilalasif@gmail.com>
2025-03-09 23:37:38 +0500

    Initial commit: Next.js portfolio setup

---
+++ app/Components/About.jsx
@@ -0,0 +1,128 @@
+import { assets, infoList, toolsData } from "@/assets/assets";
+import Image from "next/image";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+
+const About = ({ isDark }) => {
+  const [hove, setHover] = useState(null);
+  const ImagesName = ["vscode", "firebase", "mongodb", "figma", "git"];
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      id="about"
+      className=" w-full px-[12%] py-10 scroll-mt-16"
+    >
+      <motion.h4
+        className="text-center  text-lg font-Ovo"
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
+      >
+        Introduction
+      </motion.h4>
+      <motion.h2
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.5 }}
+        className="text-center  text-5xl font-Ovo "
+      >
+        About Me
+      </motion.h2>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.8 }}
+        className="flex w-full flex-col lg:flex-row items-center gap-20 my-8"
+      >
+        <motion.div
+          initial={{ opacity: 0, scale: 0.9 }}
+          whileInView={{ opacity: 1, y: 1 }}
+          transition={{ duration: 0.5 }}
+          className="w-64 sm:w-80 rounded-3xl max-w-none"
+        >
+          <Image
+            src={assets.user_image}
+            alt="user"
+            className="w-full rounded-3xl"
+          />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.6, delay: 0.8 }}
+          className="flex-1"
+        >
+          <p className="mb-10 max-w-2xl font-Ovo">
+            I am a Full stack Dev with over a decade of professional expertise
+            in the field. Throughout my career, I have had privilege of
+            collaborating with prestigious organizations, contributing to their
+            success and growth.
+          </p>
+          <motion.ul
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 0.8, delay: 1 }}
+            className="grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl"
+          >
+            {infoList.map(({ icon, iconDark, title, description }, index) => (
+              <motion.li
+                whileHover={{ scale: 1.05 }}
+                key={index}
+                className="border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1.5 duration-500 hover:shadow-black dark:border-white dark:hover:shadow-white dark:hover:bg-darkHover/50"
+              >
+                <Image
+                  src={isDark ? iconDark : icon}
+                  alt="title"
+                  className="w-7 mt-3"
+                />
+                <h3 className="my-4 font-semibold text-gray-700 dark:text-white">
+                  {title}
+                </h3>
+                <p className="text-gray-600 text-sm  dark:text-white/80">
+                  {description}
+                </p>
+              </motion.li>
+            ))}
+          </motion.ul>
+          <motion.h4
+            initial={{ y: 20, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{ duration: 1.3, delay: 0.5 }}
+            className="my-6 text-gray-700 font-Ovo font-bol dark:text-white/80"
+          >
+            Tools I Use
+          </motion.h4>
+          <motion.ul
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1.5, delay: 0.6 }}
+            className=" flex items-center gap-3 sm:gap-5"
+          >
+            {toolsData.map((tool, index) => (
+              <motion.li
+                onMouseEnter={() => setHover(index)}
+                onMouseLeave={() => setHover(null)}
+                whileHover={{ scale: 1.1 }}
+                key={index}
+                className=" flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500  "
+              >
+                {hove === index && (
+                  <div className="absolute top-full mt-2 left-1/2 -translate-x-1/2 whitespace-nowrap bg-gray-800 text-white dark:text-black dark:bg-white text-xs px-3 py-2 rounded-lg shadow-lg">
+                    {ImagesName[index]}
+                    {/* Small triangle pointing upwards */}
+                    <div className="absolute bottom-full left-1/2 -translate-x-1/2 w-0 h-0 border-l-4 border-l-transparent border-r-4 border-r-transparent border-b-4 dark:border-b-white border-b-gray-800"></div>
+                  </div>
+                )}
+                <Image src={tool} alt={tool} className="w-5 sm:w-7" />
+              </motion.li>
+            ))}
+          </motion.ul>
+        </motion.div>
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default About;
---
+++ app/Components/Contact.jsx
@@ -0,0 +1,119 @@
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+import React from "react";
+import { useState } from "react";
+import { motion } from "motion/react";
+
+const Contact = ({ isDark }) => {
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "f363c918-e052-48b0-bdda-f6253300d84b");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      id="contact"
+      className="dark:bg-none w-full px-[12%] py-10 scroll-mt-10 bg-[url('/footer-bg-color.png')] bg-no-repeat bg-center bg-[length:90%_auto]"
+    >
+      <motion.h4
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.3, duration: 0.5 }}
+        className="text-center mb-2 mt-2 text-lg font-Ovo"
+      >
+        Connect with Me
+      </motion.h4>
+      <motion.h2
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.5, duration: 0.5 }}
+        className="text-center  text-5xl font-Ovo"
+      >
+        Get in touch
+      </motion.h2>
+      <motion.p
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ delay: 0.7, duration: 0.5 }}
+        className="text-center max-w-2xl mx-auto mt-5 mb-2 font-Ovo"
+      >
+        Welcome to my web development portfolio! Explore a collection of
+        projects showcasing my expertise in front-end development.
+      </motion.p>
+      <motion.form
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ delay: 0.9, duration: 0.5 }}
+        className="max-w-2xl mx-auto"
+        onSubmit={onSubmit}
+      >
+        <div className="grid grid-cols-auto gap-6 mt-10 mb-8">
+          <motion.input
+            initial={{ x: -50, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ delay: 1.1, duration: 0.6 }}
+            name="name"
+            type="text"
+            placeholder="Enter You're Name"
+            required
+            className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/50 dark:border-white/90"
+          />
+          <motion.input
+            initial={{ x: 50, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ delay: 1.1, duration: 0.6 }}
+            name="email"
+            type="text"
+            placeholder="Enter You're Email"
+            required
+            className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white  dark:bg-darkHover/50 dark:border-white/90"
+          />
+        </div>
+        <motion.textarea
+          initial={{ y: 100, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ delay: 1.1, duration: 0.6 }}
+          name="message"
+          rows="6"
+          placeholder="Enter You're Message"
+          required
+          className="w-full p-4  outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-2  dark:bg-darkHover/50 dark:border-white/90 "
+        />
+        <motion.button
+          whileHover={{ scale: 1.15 }}
+          transition={{ duration: 0.3 }}
+          type="submit"
+          className="py-3 px-8 w-max  flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black  dark:bg-transparent dark:border-[0.5px] dark:hover:bg-darkHover"
+        >
+          Submit Now{" "}
+          <Image src={assets.right_arrow_white} alt="" className="w-4" />
+        </motion.button>
+        <p className="mt-4">{result}</p>
+      </motion.form>
+    </motion.div>
+  );
+};
+
+export default Contact;
